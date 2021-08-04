0731b5ef5877eb2cb6d740fe28f02255ed682da8
mossishahi <shahhosseini.94@gmail.com>
2021-08-04 16:09:04 +0430

    debug _loss

--- projects/modules/_loss.py
+++ projects/modules/_loss.py
@@ -5,5 +5,5 @@ class Loss:
         self.y_pred  = y_pred
 
     def cal (self):
-        loss = ((self.labels - np.log(self.y_pred)) + ((1-self.labels) - np.log(1-self.y_pred))).sum()
+        loss = (self.labels - np.log(self.y_pred)) + ((1-self.labels) - np.log(1-self.y_pred))).sum()
         return loss
